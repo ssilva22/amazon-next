1cbd9e88b6c6efafe8be10ca553bc2bbd99f6fce
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-21 17:27:20 -0400

    Checkout page finished

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,16 +2,35 @@ import React,{useState} from 'react'
 import Image from 'next/image';
 import { StarIcon } from '@heroicons/react/solid';
 import Currency from "react-currency-formatter";
+import { useDispatch } from 'react-redux';
+import { addToBasket } from '../slices/basketSlice';
 
 const MAX_RATING=5;
 const MIN_RATING=1;
 
-function Product({id,title,price,description,category,image}) {
+function Product({ id,title,price,description,category,image}) {
+    const dispatch = useDispatch();
+
     const [rating] = useState(
         Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
     );
 
     const [hasPrime]= useState(Math.random() < .5)
+
+    const addItemToBasket = () => {
+        const product= {
+            id,
+            title,
+            price,
+            rating,
+            description,
+            category,
+            image,
+            hasPrime,
+        };
+        //Sends the product as an action to the REDUX store... the basket slice
+        dispatch(addToBasket(product));
+    };
 
     return (
         <div className="relative flex flex-col m-5 bg-white z-30 p-10 ">
@@ -39,7 +58,7 @@ function Product({id,title,price,description,category,image}) {
                 </div>
             )}
 
-            <button className="mt-auto button">Add to Basket</button>
+            <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
         </div>
     )
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,8 +2,17 @@ import React from 'react'
 import Image from 'next/image';
 
 import Header from '../components/Header'
+import CheckoutProduct from '../components/CheckoutProduct';
+
+import { useSelector } from 'react-redux';
+import { selectItems, selecTotal } from '../slices/basketSlice';
+import Currency from "react-currency-formatter";
+import { useSession } from 'next-auth/client';
 
 function Checkout() {
+    const items= useSelector(selectItems);
+    const total = useSelector(selecTotal);
+    const [session]= useSession();
     return (
           <div className="bg-gray-100">
               <Header/>
@@ -19,13 +28,41 @@ function Checkout() {
              />
 
              <div className="flex flex-col p-5 space-y-10 bg-white">
-                 <h1 className="text-3xl border-b pb-4 ">Your Shopping Cart</h1>
+                 <h1 className="text-3xl border-b pb-4 ">{items.length === 0 ? "Your Cart is Empty" : "Shopping basket"}</h1>
+
+                 {items.map((item, i ) => (
+                    <CheckoutProduct
+                        key={i}
+                        id={item.id}
+                        title={item.title}
+                        rating={item.rating}
+                        price={item.price}
+                        description={item.description}
+                        category={item.category}
+                        image={item.image}
+                        hasPrime={item.hasPrime}
+                    />
+                 ))}
              </div>
             </div>
 
                {/* Right */}
-               <div>
+               <div className="flex flex-col bg-white p-10 shadow-md">
+                    {items.length > 0 && (
+                        <>
+                        <h2 className="whitespace-nowrap">Subtotal ({items.length} items):
+                        <span className="font-bold">
+                         <Currency quantity={total} currency="USD"/>
+                        </span>
+                        </h2>
 
+                        <button 
+                        disabled={!session}
+                        className={`button mt-2 ${!session && `from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed` }`}>
+                       {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+                        </button>
+                        </>
+                    )}
                </div>
               </main>
           </div>
